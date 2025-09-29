8dc575a219f3d2298101660f8826d4661d9d47e5
Redacted Text <kuharev-art@yandex.ru>
2025-09-29 14:08:44 +0300

    Broadcast control

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ socks5://host:port
 socks5://login:password@host:port
 ```
 
+### Broadcast control
+
+You can control RedProxy via adb or android broadcasts.
+
+```shell
+adb shell am broadcast -a "net.redproxy.SET_PROXY" -n net.redproxy/.MainReceiver --es proxy "socks5://login:password@host:port"
+```
+
+```shell
+adb shell am broadcast -a "net.redproxy.SET_PROXY" -n net.redproxy/.MainReceiver --es proxy "null"
+```
+
 ### Formats to be supported
 
 ```
---
+++ app/src/main/java/net/redproxy/MainReceiver.kt
@@ -0,0 +1,33 @@
+package net.redproxy
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != "net.redproxy.SET_PROXY") return
+        val proxy = intent.getStringExtra("proxy")
+        Log.e(BuildConfig.APPLICATION_ID, "Received ${proxy}")
+        if (proxy == null) return
+        GlobalScope.launch {
+            if (!isRunning()) prepareRedsocks(context)
+            if (proxy == "" || proxy == "null") { disableRedsocks(); return@launch }
+            try {
+                if (isRunning()) {
+                    setProxyRedsocks(proxy)
+                    reloadRedsocks()
+                } else {
+                    setProxyRedsocks(proxy)
+                    enableRedsocks()
+                }
+            } catch (e: RuntimeException) {
+                disableRedsocks()
+            }
+        }
+    }
+}
